5d3e08a37846f2ce7fc982bba81ccb0c2ab3709d
Jiahao <8275280+jiahao-c@users.noreply.github.com>
2022-05-20 17:37:20 -0400

    add hourly handler

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,13 +1,24 @@
-import { Handler } from "@netlify/functions";
+import { schedule, Handler } from "@netlify/functions";
+import { fetch, CookieJar } from "node-fetch-cookies";
 
-const handler: Handler = async (event, context) => {
+const cookieJar = new CookieJar("jar.json");
+const API_ENDPOINT =
+  "https://appointment.diplomatie.be/Home/AvailableDates/?officeId=172c988b-c3dc-4e87-8382-6803a40dd992&serviceIds=7fc8a115-c7e1-40f2-a46e-e21e1e77f318";
+
+interface BelgiumResponse {
+  dataObject: {
+    dates: string[];
+  };
+}
+
+const handler: Handler = schedule("@hourly", async (event, context) => {
+  const response = await fetch(cookieJar, API_ENDPOINT);
+  const data: BelgiumResponse = await response.json();
+  const dates = data.dataObject.dates.map((date) => date.split("T")[0]);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello World",
-      node_version: process.version,
-    }),
+    body: JSON.stringify(dates),
   };
-};
+});
 
 export { handler };
