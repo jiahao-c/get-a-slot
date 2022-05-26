cb2946f83bc6b4663aa2552ea20191d9c85deeb0
Jiahao <8275280+jiahao-c@users.noreply.github.com>
2022-05-25 21:42:49 -0400

    del unused code

--- netlify/functions/check_spot.ts
+++ netlify/functions/check_spot.ts
@@ -11,7 +11,7 @@ const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
 const cookieJar = new CookieJar("jar.json");
 //only checking Belgium for now
-const API_ENDPOINT =
+const BELGIUM_API_ENDPOINT =
   "https://appointment.diplomatie.be/Home/AvailableDates/?officeId=172c988b-c3dc-4e87-8382-6803a40dd992&serviceIds=7fc8a115-c7e1-40f2-a46e-e21e1e77f318";
 
 interface BelgiumResponse {
@@ -21,7 +21,7 @@ interface BelgiumResponse {
 }
 
 const handler: Handler = schedule("@hourly", async (event, context) => {
-  const response = await fetch(cookieJar, API_ENDPOINT);
+  const response = await fetch(cookieJar, BELGIUM_API_ENDPOINT);
   const data: BelgiumResponse = await response.json();
   const dates = data.dataObject.dates.map((date) => date.split("T")[0]);
 
--- netlify/functions/main.ts
+++
@@ -1,9 +0,0 @@
-import { Handler } from "@netlify/functions";
-const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: "done",
-  };
-};
-
-export { handler };
